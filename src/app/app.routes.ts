9365cd9361ddf5e14259b2d6132d87f6076230b5
stefan hafner <stefan.hafner@hftm.ch>
2024-11-09 14:25:21 +0100

    feat(lazy-loading): i try it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,21 @@
 import { Routes } from '@angular/router';
-import { BlogOverviewPageComponent } from './feature/blog-overview-page/blog-overview-page.component';
-import { BlogDetailPageComponent } from './feature/blog-detail-page/blog-detail-page.component';
+
 import { blogDetailResolver } from './feature/blog-detail-page/blog-detail-resolver';
 
 export const routes: Routes = [
   {
     path: 'blog-overview', // Route for the blog overview page
-    component: BlogOverviewPageComponent,
+    loadComponent: () =>
+      import('./feature/blog-overview-page/blog-overview-page.component').then(
+        (m) => m.BlogOverviewPageComponent,
+      ),
   },
   {
     path: 'blog-detail/:id', // Route for the blog detail page, expects an ID as parameter
-    component: BlogDetailPageComponent,
+    loadComponent: () =>
+      import('./feature/blog-detail-page/blog-detail-page.component').then(
+        (m) => m.BlogDetailPageComponent,
+      ),
     resolve: { blog: blogDetailResolver }, // Applies the resolver to fetch blog details before loading the component
   },
   {
